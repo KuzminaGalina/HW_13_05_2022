bba01cb32141e9a042380c8e4d979748453f2b74
Galina <kuzmina0497@mail.ru>
2022-05-13 23:50:57 +0300

    Добавлен алгоритм

---
+++ HW1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число  ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number / 10;
+int number2 = number1 % 10;
+
+Console.WriteLine(number2);
