93a133f7a4b31504deb6aa31be3d638a2b6b91d9
Galina <kuzmina0497@mail.ru>
2022-05-14 08:28:27 +0300

    Добавлен алгоритм

---
+++ HW3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающиую день недели и проверяет, является ли этот день недели выходным
+
+Console.WriteLine("Введите день недели (цифру от 1 до 7) ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 6 || number ==7)
+{
+    Console.WriteLine("Выходной день");
+}
+else if (number < 1 || number > 7)
+{
+    Console.WriteLine("Такого дня недели не существует)) ");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
+
